44bfa7aca761104851569824007d9d89c33c9a49
Ilya Gavrilenko <63840433+YellowFLX@users.noreply.github.com>
2022-06-02 14:40:38 +0500

    add admin_honeypot

--- yellowtodo/urls.py
+++ yellowtodo/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('realadmin/', admin.site.urls),
     path('', todoview, name='index'),
     path('', include('todolist.urls')),
     path('', include('account.urls')),
