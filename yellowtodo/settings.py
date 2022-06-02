1c09c348f4a35c8ccf29c25b9a543f26b8d7195b
Ilya Gavrilenko <63840433+YellowFLX@users.noreply.github.com>
2022-06-02 15:14:27 +0500

    add middleware

--- yellowtodo/settings.py
+++ yellowtodo/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'yellowtodo.middleware.RemoteAddrMiddleware'
 ]
 
 ROOT_URLCONF = 'yellowtodo.urls'
